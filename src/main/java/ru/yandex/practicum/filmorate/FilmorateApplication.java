86e61e316fe40c7d309111011e8c2979de73f8de
francesy <upostnikova0@yandex.ru>
2023-03-28 23:19:02 +0600

    delete unused import

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -2,13 +2,9 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.yandex.practicum.filmorate.model.User;
 
 @SpringBootApplication
 public class FilmorateApplication {
-	/**
-	 *
-	 * */
 	public static void main(String[] args) {
 		SpringApplication.run(FilmorateApplication.class, args);
 	}
