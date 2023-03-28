49b3b68fd965d90dc24dbfc7298fc66041d66475
francesy <upostnikova0@yandex.ru>
2023-03-28 23:31:45 +0600

    style: delete unused import from GenreService

--- src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
@@ -7,7 +7,6 @@ import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.storage.genre.GenreStorage;
 
 import java.util.Collection;
-import java.util.Set;
 
 @Slf4j
 @Service
