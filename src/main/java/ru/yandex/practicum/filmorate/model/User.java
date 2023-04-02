e2309e0b4c23753c116b4f0c83691681b2ef0065
francesy <upostnikova0@yandex.ru>
2023-04-02 19:42:57 +0600

    Ñstyle: remove unused import

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
