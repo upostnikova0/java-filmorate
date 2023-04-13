3c8107d619ea09099ed510ae545e34ff680523dc
Vitalya1991 <convinceall@mail.ru>
2023-04-13 19:43:17 +0300

    add-search

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -148,6 +148,7 @@ public class FilmDbStorage implements FilmStorage {
                 .directors(new ArrayList<>())
                 .build();
     }
+
     @Override
     public Collection<Film> searchFilms(Optional<String> query, List<String> by) {
         List<Film> searchedFilms = new ArrayList<>();
